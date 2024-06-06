979c621546f711c4a324a6a4b12bd40ab7c40fe2
keenzay <keenzay@gmail.com>
2024-06-06 05:25:30 -0500

    tire

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,13 +1,13 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+# Start button brings you to driving scene
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/driving_game.tscn")
-	
 
-func _on_quit_button_pressed():
+# Quitting the game
+func _on_quit_pressed():
+	print("trying to quit")
 	get_tree().quit()
