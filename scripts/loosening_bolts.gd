abdbd14e33d25d83f61ccc1f3b5455a51e67e3a7
keenzay <keenzay@gmail.com>
2024-06-10 02:35:21 -0500

    bbbbbb

--- scripts/loosening_bolts.gd
+++ scripts/loosening_bolts.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var wrench = $wrench
 @onready var bolts = [$"Bolts/BoltPic-modified",$"Bolts/BoltPic-modified2",$"Bolts/BoltPic-modified3",$"Bolts/BoltPic-modified4"]
 @onready var timer = $Timer
+@onready var carJack = $"car-jack"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,11 +17,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if $slot_object.hasItem == true: 
+		await get_tree().create_timer(0.15).timeout 
+		get_tree().change_scene_to_file("res://scenes/car_jacking.tscn")
 	pass
 
 func _on_collision_shape_2d_child_entered_tree(wrench):
 	bolts.queue_free()
 	timer.start(2)
 	
-func _on_timer_timeout():
-	get_tree().change_scene_to_file("res://scenes/car_jacking.tscn")
